a5ee3653a5cec95ab8d47e4f95dc572ccfce4861
Dylan Spies <47045235+spies36@users.noreply.github.com>
2024-03-26 22:48:42 -0500

    finishing touches

--- tests/mockRequests.ts
+++ tests/mockRequests.ts
@@ -1,48 +1,33 @@
 import { response, NextFunction, Request } from 'express';
 import { VersionedRequest } from '../types'
 
-const defaultRequest = {
-    body: {},
-    headers: {}
-} as Request
-
-function buildRequest() {
+function buildRequest(headers: Record<string, string>) {
     return {
         body: {},
-        headers: {}
+        headers: headers
     } as VersionedRequest
 }
 
-export const requestWithNoVersion = buildRequest();
+export const requestWithNoVersion = buildRequest({});
 requestWithNoVersion.originalUrl = 'google.com';
 
-export const requestWithHighestSubVersion = buildRequest();
-requestWithHighestSubVersion.headers['accept-version'] = '0.0.2';
+export const requestWithHighestSubVersion = buildRequest({ 'accept-version': '0.0.2' });
 
-export const requestWithHighestMinorVersion = buildRequest();
-requestWithHighestMinorVersion.headers['accept-version'] = '0.3.0';
+export const requestWithHighestMinorVersion = buildRequest({ 'accept-version': '0.3.0' });
 
-export const requestWithHighestMajorVersion = buildRequest();
-requestWithHighestMajorVersion.headers['accept-version'] = '3.0.0';
+export const requestWithHighestMajorVersion = buildRequest({ 'accept-version': '3.0.0' });
 
-export const requestWithHighestMajMinSubVersion = buildRequest();
-requestWithHighestMajMinSubVersion.headers['accept-version'] = '1.1.1';
+export const requestWithHighestMajMinSubVersion = buildRequest({ 'accept-version': '1.1.1' });
 
+export const requestWithMiddleSubVersion = buildRequest({ 'accept-version': '0.0.1' });
 
-export const requestWithMiddleSubVersion = buildRequest();
-requestWithMiddleSubVersion.headers['accept-version'] = '0.0.1';
+export const requestWithMiddleMinorVersion = buildRequest({ 'accept-version': '0.2.0' });
 
-export const requestWithMiddleMinorVersion = buildRequest();
-requestWithMiddleMinorVersion.headers['accept-version'] = '0.2.0';
+export const requestWithMiddleMajorVersion = buildRequest({ 'accept-version': '2.0.0' });
 
-export const requestWithMiddleMajorVersion = buildRequest();
-requestWithMiddleMajorVersion.headers['accept-version'] = '2.0.0'
+export const requestWithMiddleMajMinSubVersion = buildRequest({ 'accept-version': '1.0.0' });
 
-export const requestWithMiddleMajMinSubVersion = buildRequest();
-requestWithMiddleMajMinSubVersion.headers['accept-version'] = '1.0.0'
-
-export const requestWithNoMatchMajMinSubVersion = buildRequest();
-requestWithNoMatchMajMinSubVersion.headers['accept-version'] = '1.0.9'
+export const requestWithNoMatchMajMinSubVersion = buildRequest({ 'accept-version': '1.0.9' });
 
 export const res = response
 export const nextFunc: NextFunction = () => { };
